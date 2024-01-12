629f959b4041860aaef571d4b14b525211a8601b
lzyznl <2941649503@qq.com>
2024-01-12 13:42:50 +0800

    修复bug

--- src/main/java/com/lzy/serverproject/config/CorsConfig.java
+++ src/main/java/com/lzy/serverproject/config/CorsConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 全局跨域配置
  *
  * @author <a href="https://github.com/lilzy">程序员小刘</a>
- * @from <a href="https://lzy.icu">编程导航知识星球</a>
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
